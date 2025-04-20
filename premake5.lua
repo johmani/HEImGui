2fdd3377821207509c674592f35bfbf7b88baff4
Mohamd Johmani <mohamdaljohmani@gmail.com>
2025-04-20 15:03:11 +0300

    Update Premake

--- premake5.lua
+++ premake5.lua
@@ -8,9 +8,10 @@ project "HEImGui"
     language "C++"
     cppdialect  "C++latest"
     staticruntime "Off"
-
     targetdir ("Binaries/" .. outputdir)
     objdir ("Binaries/Intermediates/" .. outputdir)
+   
+    LinkHydra(includSourceCode, { "glfw" })
 
     files
     {
@@ -21,87 +22,25 @@ project "HEImGui"
         "*.lua",
     }
     
-    defines
-    {
-         "HE_CORE_IMPORT_SHAREDLIB",
-         "NVRHI_SHARED_LIBRARY_INCLUDE",
-         "_SILENCE_STDEXT_ARR_ITERS_DEPRECATION_WARNING",
-         "GLFW_DLL",
-    }
-    
     includedirs
     {
        "Source",
-       "%{IncludeDir.HydraEngine}",
        "%{IncludeDir.imgui}",
        "%{IncludeDir.glfw}",
     }
-    
+
     links
     {
-       "HydraEngine",
-       "glfw",
-       "nvrhi",
        "imgui",
     }
 
-    buildoptions 
-    {
-        AddCppm("std"),
-        AddCppm("HydraEngine"),
-        AddCppm("nvrhi")
-    }
+    SetupShaders(
+        { D3D11 = true, D3D12 = true, VULKAN = true },  -- api
+        "%{prj.location}/Source/Shaders",               -- sourceDir
+        "%{prj.location}/Source/HEImGui/Embeded",       -- cacheDir
+        "--header"                                      -- args
+    )
 
-     filter { "files:**.hlsl" }
-        buildcommands {
-            BuildShaders(
-                { D3D11 = true, D3D12 = true, VULKAN = true },  -- api
-                "%{prj.location}/Source/Shaders",               -- sourceDir
-                "%{prj.location}/Source/HEImGui/Embeded",       -- cacheDir
-                "--header",                                     -- args
-                {}
-            ),
-        }
-        buildoutputs { "%{wks.location}/dumy" }
-    filter {}
-    
-    filter "system:windows"
-        systemversion "latest"
-        defines 
-        {
-        	"NVRHI_HAS_D3D11",
-        	"NVRHI_HAS_D3D12",
-        	"NVRHI_HAS_VULKAN",
-        }
-
-    filter "system:linux"
-		pic "On"
-		systemversion "latest"
-        defines
-        {
-        	"NVRHI_HAS_VULKAN",
-        }
-     
-    filter "configurations:Debug"
-     	defines "HE_DEBUG"
-     	runtime "Debug"
-     	symbols "On"
-     
-    filter "configurations:Release"
-     	defines "HE_RELEASE"
-     	runtime "Release"
-     	optimize "On"
-     
-    filter "configurations:Profile"
-        includedirs { "%{IncludeDir.tracy}" }
-        defines { "HE_PROFILE", "TRACY_IMPORTS" }
-        runtime "Release"
-        optimize "On"
-
-    filter "configurations:Dist"
-     	defines "HE_DIST"
-     	runtime "Release"
-     	optimize "Speed"
-        symbols "Off"
+    SetHydraFilters()
 
 group "Plugins"
